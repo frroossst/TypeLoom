a88d97e28801e6e0e297fdeeb0d31de6fbeb9505
Adhyan H <adhyanpatel@gmail.com>
2023-08-25 22:32:31 -0600

    added basic syntax highlighting

--- typeloom/client/src/extension.ts
+++ typeloom/client/src/extension.ts
@@ -1,1 +1,18 @@
+import { createConnection, ProposedFeatures } from 'vscode-languageserver';
 
+// Create a connection to the server
+const connection = createConnection(ProposedFeatures.all);
+
+// Initialize the connection
+connection.listen();
+
+// Request type hints at a specific position
+const documentUri = 'file:///path/to/your/file.txt'; // Replace with the actual URI
+const position = { line: 10, character: 5 }; // Replace with the actual position
+connection.sendRequest('getTypeHints', {
+  textDocument: { uri: documentUri },
+  position: position,
+}).then((typeHints) => {
+  // Here, you'd update the editor UI with the received type hints
+  console.log('Received type hints:', typeHints);
+});
--- typeloom/server/src/server.ts
+++ typeloom/server/src/server.ts
@@ -1,1 +1,19 @@
+import { createConnection, ProposedFeatures } from 'vscode-languageserver';
+
+// Create a connection to the client
+const connection = createConnection(ProposedFeatures.all);
+
+// Define a request handler for type hints
+connection.onRequest('getTypeHints', (params) => {
+  const code = params.textDocument.text;
+  const position = params.position;
+
+  // Here, you'd perform type inference and return type hints
+  const typeHints = getTypeHintsForPosition(code, position);
+
+  return typeHints;
+});
+
+// Listen for the connection to initialize
+connection.listen();
 
